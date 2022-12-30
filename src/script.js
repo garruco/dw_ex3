006e020195873a5bf9ff2f956e25b00bbef96135
nijatori <garruco@live.com.pt>
2022-12-30 23:41:06 +0000

    Implementation of Website Functions

--- src/script.js
+++ src/script.js
@@ -12,3 +12,36 @@ const handleOnMouseMove = (e) => {
 for (const card of document.querySelectorAll(".card")) {
   card.onmousemove = (e) => handleOnMouseMove(e);
 }
+
+function show1() {
+  var f = document.getElementById("colapse-about1");
+  var plus1 = document.getElementById("plus1");
+
+  if (f.style.display === "none") {
+    f.style.display = "block";
+    f.style.opacity = "1";
+    f.style.animation = "1s animopacity";
+    plus1.innerHTML = "-";
+  } else {
+    f.style.display = "none";
+    f.style.opacity = "0";
+    f.style.animation = "1s reverse animopacity";
+    plus1.innerHTML = "+";
+  }
+}
+function show2() {
+  var d = document.getElementById("colapse-about2");
+  var plus2 = document.getElementById("plus2");
+
+  if (d.style.display === "none") {
+    d.style.display = "block";
+    d.style.opacity = "1";
+    d.style.animation = "1s animopacity";
+    plus2.innerHTML = "-";
+  } else {
+    d.style.display = "none";
+    d.style.opacity = "0";
+    d.style.animation = "1s reverse animopacity";
+    plus2.innerHTML = "+";
+  }
+}
